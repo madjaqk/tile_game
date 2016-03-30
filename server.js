79bc8c2d25bbcb1f9a781e794bc2062c638f0d7c
Jack Brounstein <Jack.Brounstein@yahoo.com>
2016-03-30 16:57:36 -0700

    moves css to separate stylesheet

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, "/client/views")))
+app.use(express.static(path.join(__dirname, "/client")))
 app.use(express.static(__dirname + "/node_modules"))
 
 require("./server/config/mongoose.js")
