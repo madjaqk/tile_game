c7cf04811012f090c37d8a3b586b2563656123ec
Jack Brounstein <Jack.Brounstein@yahoo.com>
2016-03-31 10:49:57 -0700

    adds score checking function

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,4 +3,5 @@ var words = require("./../controllers/words.js")
 module.exports = function(app){
 	app.get("/words", words.index) // No apostrophes after lines
 	app.get("/words/pick_words", words.pick_words)
+	app.post("/words/find_score", words.find_score)
 }
--- server/controllers/words.js
+++ server/controllers/words.js
@@ -3,6 +3,7 @@
 // var mongoose = require("mongoose")
 // var Document = mongoose.model("Document")
 var words = require("./../../wordlists/20k_legal.json")
+var scores = require("./../../wordlists/word_scores.json")
 
 // Rather than the immediate function, it might be better to do this as a constructor function or just an object literal, but that's now how I've done it thus far.
 module.exports = (function(){
@@ -22,6 +23,14 @@ module.exports = (function(){
 			for(var i = 3; i <= 6; i++){
 				output[i] = words[i][Math.floor(Math.random()*words[i].length)]
 			}
+			res.json(output)
+		},
+		find_score: function(req, res){
+			output = {}
+			for(var i=3; i<=6; i++){
+				output[i] = scores[i][req.body[i].join("")]
+			}
+			console.log("find_score output", output)
 			res.json(output)
 		}
 	}
