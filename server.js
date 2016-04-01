720fe1f61fb86799009d1e07a0b4c3095b136c67
Jack Brounstein <Jack.Brounstein@yahoo.com>
2016-04-01 12:18:11 -0700

    clean up

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 var app = express();
 
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, "/client")))
 app.use(express.static(__dirname + "/node_modules"))
